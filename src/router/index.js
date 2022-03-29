cacc66cb81ab3133d4156b5153a580fcd7d2c966
Song0129 <Song_3191@163.com>
2022-03-29 15:20:36 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,60 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    meta: {
-      title: '登录',
-    },
-    component: () => import(/* webpackChunkName: "login" */'../views/Login/Login'),
-  },
-  {
-    path: '/',
-    redirect: "/form",
-    name: 'Home',
-    component: Home,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    meta: {
-      title: 'Home',
-      requireAuth: false,
-      sideBar: true,
-    },
-    children: [
-      {
-        path: '/form',
-        name: 'form',
-        meta: {
-          title: 'Home',
-          requireAuth: false,
-          sideBar: true,
-        },
-        component: () => import(/* webpackChunkName: "form" */ '../views/Form/Form')
-      }
-    ]
-  },
-
-
-]
+	{
+		path: "/login",
+		name: "login",
+		meta: {
+			title: "登录",
+		},
+		component: () => import(/* webpackChunkName: "login" */ "../views/Login/Login"),
+	},
+	{
+		path: "/",
+		redirect: "/form",
+		name: "Home",
+		component: Home,
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		meta: {
+			title: "Home",
+			requireAuth: false,
+			sideBar: true,
+		},
+		children: [
+			{
+				path: "/form",
+				name: "form",
+				meta: {
+					title: "Home",
+					requireAuth: false,
+					sideBar: true,
+				},
+				component: () => import(/* webpackChunkName: "form" */ "../views/Form/Form"),
+			},
+			{
+				path: "/about",
+				name: "about",
+				meta: {
+					title: "About",
+					requireAuth: false,
+					sideBar: true,
+				},
+				component: () => import(/* webpackChunkName: "about" */ "../views/About/About"),
+			},
+		],
+	},
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes,
+});
 
-export default router
+export default router;
